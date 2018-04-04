49f3658b7172aeb91497bc85083dbcfc917c6ba1
Duct-and-rice <nikuinemitsubishi@gmail.com>
2018-04-04 23:42:38 +0900

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const table = [
+  {name: '明治', start: new Date(1868, 1, 25)},
+  {name: '大正', start: new Date(1912,7,30)},
+  {name: '昭和', start: new Date(1926, 12, 25)},
+  {name: '平成', start: new Date(1989, 1, 8)}
+]
+
+module.exports = table
